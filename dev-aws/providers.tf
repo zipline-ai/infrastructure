1b115218c2c6ff0ae9c427c6235ec8c8334cd705
chewy-zlai <chewys@zipline.ai>
2024-11-26 13:26:56 -0800

    Add dev tf directory so we can bring up additional instances

---
+++ dev-aws/providers.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "zipline-ai-opentofu-state-bucket"
+    key            = "opentofu-dev-${var.user}-state"
+    region         = var.region
+  }
+}
+provider "aws" {
+  region = var.region
+  profile = "default"
+
+}
+
+provider "kubernetes" {
+  host                   = module.base_setup.eks_endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.zipline_canary_eks.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.zipline_canary_eks.name]
+    command     = "aws"
+  }
+}
+
+
+
+module "base_setup" {
+  source = "../base-aws"
+
+  name = "dev-${var.user}"
+}
