da969fc535d1407dc24bf9fd321dc3f5c794c341
chewy-zlai <chewys@zipline.ai>
2024-12-17 21:46:47 -0800

    name vpc

--- base-aws/vpc.tf
+++ base-aws/vpc.tf
@@ -2,6 +2,9 @@
 resource "aws_vpc" "main" {
   cidr_block           = "172.31.0.0/16"
   enable_dns_hostnames = true
+  tags = {
+    name = "Zipline-${var.name}-VPC"
+  }
 }
 
 resource "aws_subnet" "main" {
