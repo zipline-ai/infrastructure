83d550048c07f144910e081e3bba4b1f6798153c
chewy-zlai <chewys@zipline.ai>
2024-12-18 10:37:51 -0800

    Add garbage collection to Bigtable

--- base-gcp/bigtable.tf
+++ base-gcp/bigtable.tf
@@ -13,24 +13,109 @@ resource "google_bigtable_instance" "zipline_bigtable_instance" {
 resource "google_bigtable_table" "groupby_batch" {
   instance_name = google_bigtable_instance.zipline_bigtable_instance.name
   name = "GROUPBY_BATCH"
+  column_family {
+    family = "cf"
+  }
+}
+
+resource "google_bigtable_gc_policy" "groupby_batch_gc_policy" {
+  instance_name = google_bigtable_instance.zipline_bigtable_instance.name
+  table = google_bigtable_table.groupby_batch.name
+  column_family = "cf"
+
+  mode = "UNION"
+  max_age {
+    duration = "120h"
+  }
+  max_version {
+    number = 10000
+  }
 }
 
 resource "google_bigtable_table" "groupby_streaming" {
   instance_name = google_bigtable_instance.zipline_bigtable_instance.name
   name = "GROUPBY_STREAMING"
+  column_family {
+    family = "cf"
+  }
+}
+
+resource "google_bigtable_gc_policy" "groupby_streaming_policy" {
+  instance_name = google_bigtable_instance.zipline_bigtable_instance.name
+  table = google_bigtable_table.groupby_streaming.name
+  column_family = "cf"
+
+  mode = "UNION"
+  max_age {
+    duration = "120h"
+  }
+  max_version {
+    number = 10000
+  }
 }
 
 resource "google_bigtable_table" "tile_summaries" {
   instance_name = google_bigtable_instance.zipline_bigtable_instance.name
   name = "TILE_SUMMARIES"
+  column_family {
+    family = "cf"
+  }
+}
+
+resource "google_bigtable_gc_policy" "tile_summaries_gc_policy" {
+  instance_name = google_bigtable_instance.zipline_bigtable_instance.name
+  table = google_bigtable_table.tile_summaries.name
+  column_family = "cf"
+
+  mode = "UNION"
+  max_age {
+    duration = "120h"
+  }
+  max_version {
+    number = 10000
+  }
 }
 
 resource "google_bigtable_table" "chronon_metadata" {
   instance_name = google_bigtable_instance.zipline_bigtable_instance.name
   name = "CHRONON_METADATA"
+  column_family {
+    family = "cf"
+  }
+}
+
+resource "google_bigtable_gc_policy" "chronon_metadata_gc_policy" {
+  instance_name = google_bigtable_instance.zipline_bigtable_instance.name
+  table = google_bigtable_table.chronon_metadata.name
+  column_family = "cf"
+
+  mode = "UNION"
+  max_age {
+    duration = "120h"
+  }
+  max_version {
+    number = 10000
+  }
 }
 
 resource "google_bigtable_table" "entity_keys_by_team" {
   instance_name = google_bigtable_instance.zipline_bigtable_instance.name
   name = "ENTITY_KEYS_BY_TEAM"
+  column_family {
+    family = "cf"
+  }
+}
+
+resource "google_bigtable_gc_policy" "entity_keys_by_team_gc_policy" {
+  instance_name = google_bigtable_instance.zipline_bigtable_instance.name
+  table = google_bigtable_table.entity_keys_by_team.name
+  column_family = "cf"
+
+  mode = "UNION"
+  max_age {
+    duration = "120h"
+  }
+  max_version {
+    number = 10000
+  }
 }
