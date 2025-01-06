dc0dda9c81fa87abe21bc4e8909b7ddda95ecb96
chewy-zlai <chewys@zipline.ai>
2025-01-06 12:39:04 -0800

    rename gcs bucket to zipline-, and rename zipline name variable to  for clarity

--- base-gcp/gcs.tf
+++ base-gcp/gcs.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "zipline" {
-  name     = "zipline"
+  name     = "zipline-${var.name}"
   location = var.region
   uniform_bucket_level_access = true
 }
--- zipline-gcp/variables.tf
+++ zipline-gcp/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
 }
 
-variable "name" {
+variable "customer_name" {
 }
