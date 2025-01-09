baec375ac0a5afb58f57262bd05e98eccfd9d804
David Han <david@zipline.ai>
2025-01-09 12:01:23 -0500

    vs code didn't save

--- base-gcp/gcs.tf
+++ base-gcp/gcs.tf
@@ -1,9 +1,11 @@
 resource "google_storage_bucket" "zipline" {
+  count    = var.create_warehouse_bucket ? 1 : 0  
   name     = "zipline-warehouse-${lower(var.customer_name)}"
   location = var.region
   uniform_bucket_level_access = true
 }
 
+# only expecting canary to be created
 resource "google_storage_bucket" "zipline_artifacts_bucket" {
   name     = "zipline-artifacts-${lower(var.customer_name)}"
   location = var.region
