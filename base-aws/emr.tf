7ccc20054ec2325822f44a09c543c2cf63e27cbc
chewy-zlai <chewys@zipline.ai>
2025-02-20 11:03:23 -0800

    Simplify AWS permissions

--- base-aws/emr.tf
+++ base-aws/emr.tf
@@ -62,72 +62,10 @@ resource "aws_iam_role" "iam_emr_service_role" {
   name               = "zipline_${var.customer_name}_emr_service_role"
   assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
 }
-data "aws_iam_policy_document" "iam_emr_service_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "ec2:AuthorizeSecurityGroupEgress",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CancelSpotInstanceRequests",
-      "ec2:CreateNetworkInterface",
-      "ec2:CreateSecurityGroup",
-      "ec2:CreateTags",
-      "ec2:DeleteNetworkInterface",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DeleteTags",
-      "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeAccountAttributes",
-      "ec2:DescribeDhcpOptions",
-      "ec2:DescribeInstanceStatus",
-      "ec2:DescribeInstances",
-      "ec2:DescribeKeyPairs",
-      "ec2:DescribeNetworkAcls",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribePrefixLists",
-      "ec2:DescribeRouteTables",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSpotInstanceRequests",
-      "ec2:DescribeSpotPriceHistory",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeVpcAttribute",
-      "ec2:DescribeVpcEndpoints",
-      "ec2:DescribeVpcEndpointServices",
-      "ec2:DescribeVpcs",
-      "ec2:DetachNetworkInterface",
-      "ec2:ModifyImageAttribute",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:RequestSpotInstances",
-      "ec2:RevokeSecurityGroupEgress",
-      "ec2:RunInstances",
-      "ec2:TerminateInstances",
-      "ec2:DeleteVolume",
-      "ec2:DescribeVolumeStatus",
-      "ec2:DescribeVolumes",
-      "ec2:DetachVolume",
-      "glue:*",
-      "iam:GetRole",
-      "iam:GetRolePolicy",
-      "iam:ListInstanceProfiles",
-      "iam:ListRolePolicies",
-      "iam:PassRole",
-      "s3:CreateBucket",
-      "s3:Get*",
-      "s3:List*",
-      "sdb:BatchPutAttributes",
-      "sdb:Select",
-      "sqs:CreateQueue",
-      "sqs:Delete*",
-      "sqs:GetQueue*",
-      "sqs:PurgeQueue",
-      "sqs:ReceiveMessage",
-    ]
-    resources = ["*"]
-  }
-}
-resource "aws_iam_role_policy" "iam_emr_service_policy" {
-  name   = "zipline_${var.customer_name}_emr_service_policy"
+
+resource "aws_iam_role_policy_attachment" "iam_emr_service_policy" {
   role   = aws_iam_role.iam_emr_service_role.id
-  policy = data.aws_iam_policy_document.iam_emr_service_policy.json
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEMRServicePolicy_v2"
 }
 
 # IAM Role for EC2 Instance Profile
@@ -154,29 +92,24 @@ data "aws_iam_policy_document" "iam_emr_profile_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "cloudwatch:*",
-      "dynamodb:*",
-      "ec2:Describe*",
-      "elasticmapreduce:Describe*",
-      "elasticmapreduce:ListBootstrapActions",
-      "elasticmapreduce:ListClusters",
-      "elasticmapreduce:ListInstanceGroups",
-      "elasticmapreduce:ListInstances",
-      "elasticmapreduce:ListSteps",
-      "glue:*",
-      "kinesis:CreateStream",
-      "kinesis:DeleteStream",
-      "kinesis:DescribeStream",
-      "kinesis:GetRecords",
-      "kinesis:GetShardIterator",
-      "kinesis:MergeShards",
-      "kinesis:PutRecord",
-      "kinesis:SplitShard",
-      "rds:Describe*",
-      "s3:*",
-      "sdb:*",
-      "sns:*",
-      "sqs:*",
+      "cloudwatch:PutMetricData",
+      "dynamodb:DescribeTable",
+      "dynamodb:GetRecords",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetPartitions",
+      "glue:GetTableVersion",
+      "glue:GetTableVersions",
+      "glue:GetDatabases",
+      "glue:GetDatabase",
+      "s3:ListBucket",
+      "s3:ListObjects",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:UpdateObject",
     ]
     resources = ["*"]
   }
