d09ef2ae0941c5931670d94ac1c07e0e6862493a
chewy-zlai <chewys@zipline.ai>
2025-02-24 11:48:12 -0800

    Readd emr service role policy and use it for autoscaling as well

--- base-aws/emr.tf
+++ base-aws/emr.tf
@@ -32,7 +32,8 @@ resource "aws_emr_cluster" "emr_cluster" {
   core_instance_group {
     instance_type = "m5.xlarge"
   }
-  service_role = aws_iam_role.iam_emr_service_role.arn
+  service_role     = aws_iam_role.iam_emr_service_role.arn
+  autoscaling_role = aws_iam_role.iam_emr_service_role.arn
 }
 
 resource "aws_emr_managed_scaling_policy" "zipline_scaling" {
@@ -62,10 +63,61 @@ resource "aws_iam_role" "iam_emr_service_role" {
   name               = "zipline_${var.customer_name}_emr_service_role"
   assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
 }
-
-resource "aws_iam_role_policy_attachment" "iam_emr_service_policy" {
-  role       = aws_iam_role.iam_emr_service_role.id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEMRServicePolicy_v2"
+data "aws_iam_policy_document" "iam_emr_service_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateTags",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteTags",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeInstances",
+      "ec2:DescribeKeyPairs",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribePrefixLists",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:DescribeSpotPriceHistory",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribeVpcs",
+      "ec2:DetachNetworkInterface",
+      "ec2:ModifyImageAttribute",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:RequestSpotInstances",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RunInstances",
+      "ec2:TerminateInstances",
+      "ec2:DeleteVolume",
+      "ec2:DescribeVolumeStatus",
+      "ec2:DescribeVolumes",
+      "ec2:DetachVolume",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:ListInstanceProfiles",
+      "iam:ListRolePolicies",
+      "iam:PassRole",
+    ]
+    resources = ["*"]
+  }
+}
+resource "aws_iam_role_policy" "iam_emr_service_policy" {
+  name   = "zipline_${var.customer_name}_emr_service_policy"
+  role   = aws_iam_role.iam_emr_service_role.id
+  policy = data.aws_iam_policy_document.iam_emr_service_policy.json
 }
 
 # IAM Role for EC2 Instance Profile
