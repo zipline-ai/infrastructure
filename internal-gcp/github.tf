454de044c374fbe1627a429c2701867f27fa161a
chewy-zlai <chewys@zipline.ai>
2025-03-04 18:59:33 -0800

    fix account references for gcp github resources

--- internal-gcp/github.tf
+++ internal-gcp/github.tf
@@ -3,11 +3,13 @@
 # This allows Github Actions to authenticate to GCP and perform actions
 resource "google_iam_workload_identity_pool" "github" {
   workload_identity_pool_id = "github-actions"
+  display_name              = "GitHub Workload Identity Pool"
+  description               = "Workload Identity Pool for GitHub Actions"
 }
 
 # This creates a workload identity pool provider for Github Actions
 resource "google_iam_workload_identity_pool_provider" "github" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
   workload_identity_pool_provider_id = "github"
   display_name                       = "Zipline Github Actions"
   attribute_mapping = {
@@ -22,6 +24,9 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
+  depends_on = [
+    google_iam_workload_identity_pool.github
+  ]
 }
 
 # This creates a service account for Github Actions
@@ -30,8 +35,14 @@ resource "google_service_account" "github" {
   display_name = "Zipline Github Actions"
 }
 
+data "google_project" "internal_project" {
+}
+
 resource "google_service_account_iam_member" "github_actions" {
   service_account_id = google_service_account.github.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${data.google_project.internal_project.project_id}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github.name}/attribute.repository/zipline-ai/chronon"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/zipline-ai/chronon"
+  depends_on = [
+    google_iam_workload_identity_pool_provider.github
+  ]
 }
