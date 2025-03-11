b202aaa3191b679a4bec1b08c375c781663cdee4
chewy-zlai <chewys@zipline.ai>
2025-03-11 16:41:43 -0700

    Grant github-actions access to zipline-warehouse-canary

---
+++ canary-aws/github.tf
@@ -0,0 +1,32 @@
+data "aws_iam_role" "iam_github_actions_role" {
+  name = "github_actions"
+}
+
+data "aws_s3_bucket" "zipline_warehouse_bucket" {
+  bucket = "zipline-warehouse-${lower(var.customer_name)}"
+}
+
+data "aws_iam_policy_document" "github_actions_s3_access" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_iam_role.iam_github_actions_role.arn]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "${data.aws_s3_bucket.zipline_warehouse_bucket.arn}",
+      "${data.aws_s3_bucket.zipline_warehouse_bucket.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "github_actions_s3_access" {
+  bucket = data.aws_s3_bucket.zipline_warehouse_bucket.id
+  policy = data.aws_iam_policy_document.github_actions_s3_access.json
+}
