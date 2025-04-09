768a9f8dcf01086af9845bf5ca930a95385b6aa0
chewy-zlai <chewys@zipline.ai>
2025-04-09 16:14:17 -0700

    bump back up to 16

--- base-gcp/dataproc.tf
+++ base-gcp/dataproc.tf
@@ -88,7 +88,7 @@ resource "google_dataproc_cluster" "zipline_dataproc" {
       }
     }
     worker_config {
-      machine_type = "n1-highmem-8"
+      machine_type = "n1-highmem-16"
       disk_config {
         boot_disk_type    =  "pd-standard"
         boot_disk_size_gb =  64
