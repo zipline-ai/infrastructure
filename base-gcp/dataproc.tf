7519d3d7f071df666358f105be114e900f2e6c6d
chewy-zlai <chewys@zipline.ai>
2025-05-28 12:59:50 -0700

    remove automatic provider section

--- base-gcp/dataproc.tf
+++ base-gcp/dataproc.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.34.0"
-    }
-  }
-}
 resource "google_service_account" "dataproc_sa" {
   account_id   = "dataproc"
   display_name = "Dataproc SA"
