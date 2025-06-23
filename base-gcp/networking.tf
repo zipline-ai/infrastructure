5395f50c5204397f6f41f5a8bb284d2afe2d5ecb
chewy-zlai <chewys@zipline.ai>
2025-06-23 14:04:59 -0700

    Make priority of allowing dataproc access higher than rule restricting access

--- base-gcp/networking.tf
+++ base-gcp/networking.tf
@@ -17,35 +17,35 @@ resource "google_compute_firewall" "allow_iap_to_dataproc" {
 
 # Deny all other ingress traffic to Dataproc nodes
 resource "google_compute_firewall" "deny_other_ingress_to_dataproc" {
-  name      = "deny-other-ingress-to-dataproc"
-  network   = "default"
-  direction = "INGRESS"
+  name          = "deny-other-ingress-to-dataproc"
+  network       = "default"
+  direction     = "INGRESS"
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["dataproc-node"]
-  priority = 999
+  target_tags   = ["dataproc-node"]
+  priority      = 999
   deny {
     protocol = "all"
   }
 }
 
 resource "google_compute_firewall" "allow_access_from_dataproc_instances" {
-    name          = "allow-access-from-dataproc-instances"
-    network       = "default"
-    direction     = "INGRESS"
-    source_ranges = ["10.128.0.0/9"]
-    allow {
-        protocol = "tcp"
-        ports    = ["0-65535"]
-    }
-    allow {
-        protocol = "udp"
-        ports    = ["0-65535"]
-    }
-    allow {
-        protocol = "icmp"
-    }
-    target_tags = ["dataproc-node"]
-    priority = 999
+  name          = "allow-access-from-dataproc-instances"
+  network       = "default"
+  direction     = "INGRESS"
+  source_ranges = ["10.128.0.0/9"]
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  target_tags = ["dataproc-node"]
+  priority    = 998
 }
 
 # IAM binding for IAP access
