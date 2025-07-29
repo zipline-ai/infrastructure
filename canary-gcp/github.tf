59469e288ac0efb4dbbc8fc22b325f7c9a2afff1
chewy-zlai <chewys@zipline.ai>
2025-07-29 14:55:09 -0700

    rename resource

--- canary-gcp/github.tf
+++ canary-gcp/github.tf
@@ -114,7 +114,7 @@ resource "google_cloud_run_service_iam_member" "github_cloud_run_invoker" {
   ]
 }
 
-resource "google_project_iam_member" "github_bigtable_user" {
+resource "google_project_iam_member" "github_bigtable_admin" {
   project = data.google_project.internal_project.project_id
   role    = "roles/bigtable.admin"
   member  = "serviceAccount:${google_service_account.github.email}"
