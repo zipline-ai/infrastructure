5812ff58f6d96ffb35368349f4589f46bbe14776
chewy-zlai <chewys@zipline.ai>
2025-08-28 00:33:39 -0700

    set to current zipline version

--- canary-gcp/modules.tf
+++ canary-gcp/modules.tf
@@ -4,6 +4,6 @@ module "base_setup" {
   customer_name = "canary"
 
   artifact_prefix = "gs://zipline-canary-artifacts"
-  zipline_version = "latest"
+  zipline_version = "64ed58f364b5951f3dff1dc27a7c3a66a07682cf"
   table_partitions_dataset = google_bigtable_table.table_partitions_ci.name
 }
