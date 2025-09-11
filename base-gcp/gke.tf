2ddbc0939683f1cfc59db30e9a10426d477186c6
chewy-zlai <chewys@zipline.ai>
2025-09-11 13:30:02 -0700

    Enable IAP for orchestration-hub in GKE

--- base-gcp/gke.tf
+++ base-gcp/gke.tf
@@ -292,6 +292,7 @@ CLI Access:
 To use the CLI, add the following to ENVIRONMENT_VARIABLES in teams.py:
 "FRONTEND_URL": "${var.zipline_ui_domain != "" ? "https://${var.zipline_ui_domain}" : "https://${google_compute_global_address.orchestration_ui_ip.address}.nip.io"}"
 "HUB_URL": "${var.hub_domain != "" ? "https://${var.hub_domain}" : "https://${google_compute_global_address.orchestration_hub_ip.address}.nip.io"}"
+"SA_NAME": "${google_service_account.zipline_user_sa.name}"
 
 EOT
 }
