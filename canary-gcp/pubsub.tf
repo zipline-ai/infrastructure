acb54401177b943ad0004fc33e25f2287a1b4002
Cristian Figueroa <cristian@zipline.ai>
2025-09-25 09:56:14 -0700

    [Canary] Terraform for landing fetch data in bigquery (#89)

--- canary-gcp/pubsub.tf
+++ canary-gcp/pubsub.tf
@@ -52,3 +52,78 @@ resource "google_pubsub_topic" "canary_logging_ooc" {
     encoding = "BINARY"
   }
 }
+
+################################################################
+# BigQuery Dataset and Table for Logging
+
+resource "google_bigquery_dataset" "data" {
+  dataset_id  = "data"
+  description = "Dataset for storing Chronon out-of-core responses"
+  location    = var.region
+  project     = data.google_project.zipline.project_id
+
+  delete_contents_on_destroy = false
+}
+
+resource "google_bigquery_table" "loggable_response" {
+  dataset_id = google_bigquery_dataset.data.dataset_id
+  table_id   = "loggable_response"
+  project    = data.google_project.zipline.project_id
+
+  schema = jsonencode([
+    {
+      name = "keyBytes"
+      type = "BYTES"
+      mode = "NULLABLE"
+    },
+    {
+      name = "valueBytes"
+      type = "BYTES"
+      mode = "NULLABLE"
+    },
+    {
+      name = "joinName"
+      type = "STRING"
+      mode = "NULLABLE"
+    },
+    {
+      name = "tsMillis"
+      type = "INTEGER"
+      mode = "NULLABLE"
+    },
+    {
+      name = "schemaHash"
+      type = "STRING"
+      mode = "NULLABLE"
+    }
+  ])
+
+  deletion_protection = false
+}
+
+################################################################
+# Pub/Sub Subscription for BigQuery Writing
+
+resource "google_pubsub_subscription" "canary_logging_bigquery" {
+  name    = "canary-logging-bigquery-sub"
+  topic   = google_pubsub_topic.canary_logging_ooc.name
+  project = data.google_project.zipline.project_id
+
+  ack_deadline_seconds       = 10
+  message_retention_duration = "604800s" # 7 days
+
+  bigquery_config {
+    table = "${data.google_project.zipline.project_id}.${google_bigquery_dataset.data.dataset_id}.${google_bigquery_table.loggable_response.table_id}"
+    use_topic_schema = true
+    drop_unknown_fields = false
+    write_metadata = false
+  }
+
+  expiration_policy {
+    ttl = "2678400s" # 31 days
+  }
+
+  retry_policy {
+    minimum_backoff = "0s"
+  }
+}
