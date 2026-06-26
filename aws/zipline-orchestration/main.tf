2eb5f25ac3454ac60cffea6674163e7e13bb0192
thomaschow <thomas@zipline.ai>
2026-06-25 19:16:38 -0700

    Fold orchestration provider wiring into wrappers
    Co-Authored-By: thomaschow <thomaschow369@gmail.com>

--- aws/zipline-orchestration/main.tf
+++ aws/zipline-orchestration/main.tf
@@ -8,6 +8,33 @@ variable "aws" {
   type        = any
 }
 
+provider "aws" {
+  region              = local.aws.region
+  allowed_account_ids = local.aws.account_id == "" ? null : [local.aws.account_id]
+}
+
+data "aws_eks_cluster" "this" {
+  name = local.aws.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = local.aws.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
 resource "terraform_data" "configuration_validation" {
   input = {
     auth_enabled = local.auth_enabled
