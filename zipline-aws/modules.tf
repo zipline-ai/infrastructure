86346ff3f30590f1f9387967ccac2c89e9e0cfa6
thomaschow <thomas@zipline.ai>
2026-06-26 17:28:12 -0700

    Remove legacy infrastructure directories
    Co-Authored-By: thomaschow <thomaschow369@gmail.com>

--- zipline-aws/modules.tf
+++
@@ -1,7 +0,0 @@
-
-module "base_setup" {
-  source = "../base-aws"
-
-  customer_name = var.customer_name
-  region        = var.region
-}
--- zipline-gcp/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "customer_name" {}
-variable "project" {}
-variable "region" {}
-variable "personnel_email" {}
-variable "bigtable_zone" {}
